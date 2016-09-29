e9be82176ed0179a69e7b9fc501c864e07465756
Chuck Greb <charles.greb@gmail.com>
2016-09-29 01:40:53 -0400

    Moves input validation into LoginForm

--- app/src/main/java/com/example/ecgreb/mvpc/controller/LoginActivity.java
+++ app/src/main/java/com/example/ecgreb/mvpc/controller/LoginActivity.java
@@ -2,20 +2,14 @@ package com.example.ecgreb.mvpc.controller;
 
 import com.example.ecgreb.mvpc.R;
 import com.example.ecgreb.mvpc.presenter.LoginPresenter;
+import com.example.ecgreb.mvpc.view.LoginForm;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 /**
  * A login screen that offers login via email/password.
@@ -23,10 +17,8 @@ import android.widget.TextView;
 public class LoginActivity extends AppCompatActivity implements LoginController {
 
   // UI references.
-  private AutoCompleteTextView mEmailView;
-  private EditText mPasswordView;
   private View mProgressView;
-  private View mLoginFormView;
+  private LoginForm mLoginFormView;
 
   private LoginPresenter loginPresenter;
 
@@ -35,28 +27,13 @@ public class LoginActivity extends AppCompatActivity implements LoginController 
     setContentView(R.layout.activity_login);
     loginPresenter = new LoginPresenter(this);
 
-    // Set up the login form.
-    mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
-
-    mPasswordView = (EditText) findViewById(R.id.password);
-    mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-      @Override public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-        if (id == R.id.login || id == EditorInfo.IME_NULL) {
-          attemptLogin();
-          return true;
-        }
-        return false;
-      }
-    });
-
-    Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-    mEmailSignInButton.setOnClickListener(new OnClickListener() {
-      @Override public void onClick(View view) {
+    mLoginFormView = (LoginForm) findViewById(R.id.login_form);
+    mLoginFormView.setOnClickListener(new OnClickListener() {
+      @Override public void onClick(View v) {
         attemptLogin();
       }
     });
 
-    mLoginFormView = findViewById(R.id.login_form);
     mProgressView = findViewById(R.id.login_progress);
   }
 
@@ -66,54 +43,7 @@ public class LoginActivity extends AppCompatActivity implements LoginController 
    * errors are presented and no actual login attempt is made.
    */
   private void attemptLogin() {
-    // Reset errors.
-    mEmailView.setError(null);
-    mPasswordView.setError(null);
-
-    // Store values at the time of the login attempt.
-    String email = mEmailView.getText().toString();
-    String password = mPasswordView.getText().toString();
-
-    boolean cancel = false;
-    View focusView = null;
-
-    // Check for a valid password, if the user entered one.
-    if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-      mPasswordView.setError(getString(R.string.error_invalid_password));
-      focusView = mPasswordView;
-      cancel = true;
-    }
-
-    // Check for a valid email address.
-    if (TextUtils.isEmpty(email)) {
-      mEmailView.setError(getString(R.string.error_field_required));
-      focusView = mEmailView;
-      cancel = true;
-    } else if (!isEmailValid(email)) {
-      mEmailView.setError(getString(R.string.error_invalid_email));
-      focusView = mEmailView;
-      cancel = true;
-    }
-
-    if (cancel) {
-      // There was an error; don't attempt login and focus the first
-      // form field with an error.
-      focusView.requestFocus();
-    } else {
-      // Show a progress spinner, and kick off a background task to
-      // perform the user login attempt.
-      loginPresenter.onLoginButtonClick(email, password);
-    }
-  }
-
-  private boolean isEmailValid(String email) {
-    //TODO: Replace this with your own logic
-    return email.contains("@");
-  }
-
-  private boolean isPasswordValid(String password) {
-    //TODO: Replace this with your own logic
-    return password.length() > 4;
+    loginPresenter.onLoginButtonClick(mLoginFormView.getEmail(), mLoginFormView.getPassword());
   }
 
   /**
@@ -154,25 +84,12 @@ public class LoginActivity extends AppCompatActivity implements LoginController 
   @Override public void showError() {
     runOnUiThread(new Runnable() {
       @Override public void run() {
-        showErrorInternal();
+        mLoginFormView.displayLoginError();
       }
     });
-  }
-
-  private void showErrorInternal() {
-    mPasswordView.setError(getString(R.string.error_incorrect_password));
-    mPasswordView.requestFocus();
   }
 
   @Override public void showSuccess() {
-    runOnUiThread(new Runnable() {
-      @Override public void run() {
-        showSuccessInternal();
-      }
-    });
-  }
-
-  private void showSuccessInternal() {
     finish();
   }
 }
--- app/src/main/java/com/example/ecgreb/mvpc/view/LoginForm.java
+++ app/src/main/java/com/example/ecgreb/mvpc/view/LoginForm.java
@@ -3,13 +3,22 @@ package com.example.ecgreb.mvpc.view;
 import com.example.ecgreb.mvpc.R;
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ScrollView;
 
 import static android.content.Context.LAYOUT_INFLATER_SERVICE;
 
 public class LoginForm extends ScrollView {
+
+  private AutoCompleteTextView mEmailView;
+  private EditText mPasswordView;
+
   public LoginForm(Context context) {
     super(context);
     init();
@@ -28,5 +37,79 @@ public class LoginForm extends ScrollView {
   private void init() {
     ((LayoutInflater) getContext().getSystemService(LAYOUT_INFLATER_SERVICE))
         .inflate(R.layout.login_form, this, true);
+
+    mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+    mPasswordView = (EditText) findViewById(R.id.password);
+
+    Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+    mEmailSignInButton.setOnClickListener(new OnClickListener() {
+      @Override public void onClick(View view) {
+        validateInput();
+      }
+    });
+  }
+
+  private void validateInput() {
+    // Reset errors.
+    mEmailView.setError(null);
+    mPasswordView.setError(null);
+
+    // Store values at the time of the login attempt.
+    String email = mEmailView.getText().toString();
+    String password = mPasswordView.getText().toString();
+
+    boolean cancel = false;
+    View focusView = null;
+
+    // Check for a valid password, if the user entered one.
+    if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+      mPasswordView.setError(getContext().getString(R.string.error_invalid_password));
+      focusView = mPasswordView;
+      cancel = true;
+    }
+
+    // Check for a valid email address.
+    if (TextUtils.isEmpty(email)) {
+      mEmailView.setError(getContext().getString(R.string.error_field_required));
+      focusView = mEmailView;
+      cancel = true;
+    } else if (!isEmailValid(email)) {
+      mEmailView.setError(getContext().getString(R.string.error_invalid_email));
+      focusView = mEmailView;
+      cancel = true;
+    }
+
+    if (cancel) {
+      // There was an error; don't attempt login and focus the first
+      // form field with an error.
+      focusView.requestFocus();
+    } else {
+      // Show a progress spinner, and kick off a background task to
+      // perform the user login attempt.
+      callOnClick();
+    }
+  }
+
+  private boolean isEmailValid(String email) {
+    //TODO: Replace this with your own logic
+    return email.contains("@");
+  }
+
+  private boolean isPasswordValid(String password) {
+    //TODO: Replace this with your own logic
+    return password.length() > 4;
+  }
+
+  public String getEmail() {
+    return mEmailView.getText().toString();
+  }
+
+  public String getPassword() {
+    return mPasswordView.getText().toString();
+  }
+
+  public void displayLoginError() {
+    mPasswordView.setError(getContext().getString(R.string.error_incorrect_password));
+    mPasswordView.requestFocus();
   }
 }
