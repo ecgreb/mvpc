0aa021769bf503e8f3efa9aee89996db81a64b1f
Chuck Greb <charles.greb@gmail.com>
2016-11-14 17:57:52 -0500

    Inject `AccountValidator` into `LoginPresenter`; let Dagger manager singleton instance

--- app/src/main/java/com/example/ecgreb/mvpc/LoginModule.java
+++ app/src/main/java/com/example/ecgreb/mvpc/LoginModule.java
@@ -1,5 +1,6 @@
 package com.example.ecgreb.mvpc;
 
+import com.example.ecgreb.mvpc.model.AccountValidator;
 import com.example.ecgreb.mvpc.presenter.LoginPresenter;
 
 import javax.inject.Singleton;
@@ -8,7 +9,11 @@ import dagger.Module;
 import dagger.Provides;
 
 @Module public class LoginModule {
-  @Provides @Singleton LoginPresenter providerLoginPresenter() {
-    return LoginPresenter.getInstance();
+  @Provides @Singleton LoginPresenter provideLoginPresenter(AccountValidator accountValidator) {
+    return new LoginPresenter(accountValidator);
+  }
+
+  @Provides @Singleton AccountValidator provideAccountValidator() {
+    return new AccountValidator();
   }
 }
